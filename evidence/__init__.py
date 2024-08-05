d046c7aafa161c7f14f5f79610e47188facf9502
James Stevenson <james.stevenson@nationwidechildrens.org>
2024-08-05 08:32:54 -0400

    fix: correctly configure logging (#49)

--- evidence/__init__.py
+++ evidence/__init__.py
@@ -1,26 +1,6 @@
 """The VICC library for normalizing evidence"""
 from pathlib import Path
 from os import environ
-import logging
 
 APP_ROOT = Path(__file__).resolve().parents[0]
 DATA_DIR_PATH = environ.get("DATA_DIR_PATH", APP_ROOT / "data")
-
-logging.basicConfig(
-    filename="evidence-normalizer.log",
-    format="[%(asctime)s] - %(name)s - %(levelname)s : %(message)s"
-)
-logger = logging.getLogger("evidence")
-logger.setLevel(logging.DEBUG)
-
-logging.getLogger("boto3").setLevel(logging.INFO)
-logging.getLogger("botocore").setLevel(logging.INFO)
-logging.getLogger("python_jsonschema_objects").setLevel(logging.INFO)
-logging.getLogger("swagger_spec_validator").setLevel(logging.INFO)
-logging.getLogger("urllib3").setLevel(logging.INFO)
-logging.getLogger("s3transfer.utils").setLevel(logging.INFO)
-logging.getLogger("s3transfer.tasks").setLevel(logging.INFO)
-logging.getLogger("s3transfer.futures").setLevel(logging.INFO)
-logging.getLogger("hgvs.parser").setLevel(logging.INFO)
-logging.getLogger("biocommons.seqrepo.seqaliasdb.seqaliasdb").setLevel(logging.INFO)
-logging.getLogger("biocommons.seqrepo.fastadir.fastadir").setLevel(logging.INFO)
