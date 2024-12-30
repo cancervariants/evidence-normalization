f2e860ab143677eb6e1a41b8c8e1c5b7114d5b2b
Kori Kuzma <korikuzma@gmail.com>
2024-12-30 12:13:21 -0500

    style: replace flake8 with ruff
    close #37

--- evidence/__init__.py
+++ evidence/__init__.py
@@ -1,6 +1,7 @@
 """The VICC library for normalizing evidence"""
-from pathlib import Path
+
 from os import environ
+from pathlib import Path
 
 APP_ROOT = Path(__file__).resolve().parents[0]
 DATA_DIR_PATH = environ.get("DATA_DIR_PATH", APP_ROOT / "data")
--- evidence/data_sources/cbioportal.py
+++ evidence/data_sources/cbioportal.py
@@ -1,17 +1,15 @@
 """Module for accessing python client for cBioPortal."""
-import logging
-from typing import Optional
-from pathlib import Path
-import shutil
-from os import remove
+
 import csv
+import logging
+import shutil
+from pathlib import Path
 
 import boto3
 
 from evidence import DATA_DIR_PATH
 from evidence.data_sources.base import DownloadableDataSource
-from evidence.schemas import SourceDataType, SourceMeta, Response, Sources
-
+from evidence.schemas import Response, SourceDataType, SourceMeta, Sources
 
 _logger = logging.getLogger(__name__)
 
@@ -20,11 +18,12 @@ class CBioPortal(DownloadableDataSource):
     """cBioPortal class."""
 
     def __init__(
-        self, data_url: str = "https://cbioportal-datahub.s3.amazonaws.com/msk_impact_2017.tar.gz",  # noqa: E501
+        self,
+        data_url: str = "https://cbioportal-datahub.s3.amazonaws.com/msk_impact_2017.tar.gz",
         src_dir_path: Path = DATA_DIR_PATH / "cbioportal",
-        transformed_mutations_data_path: Optional[Path] = None,
-        transformed_case_lists_data_path: Optional[Path] = None,
-        ignore_transformed_data: bool = False
+        transformed_mutations_data_path: Path | None = None,
+        transformed_case_lists_data_path: Path | None = None,
+        ignore_transformed_data: bool = False,
     ) -> None:
         """Initialize cbioportal class
 
@@ -42,13 +41,14 @@ class CBioPortal(DownloadableDataSource):
         """
         super().__init__(data_url, src_dir_path, ignore_transformed_data)
         self.source_meta = SourceMeta(
-            label=Sources.CBIOPORTAL,
-            version="msk_impact_2017"
+            label=Sources.CBIOPORTAL, version="msk_impact_2017"
         )
         self.transformed_mutations_data_path = self.get_transformed_data_path(
-            transformed_mutations_data_path, SourceDataType.CBIOPORTAL_MUTATIONS)
+            transformed_mutations_data_path, SourceDataType.CBIOPORTAL_MUTATIONS
+        )
         self.transformed_case_lists_data_path = self.get_transformed_data_path(
-            transformed_case_lists_data_path, SourceDataType.CBIOPORTAL_CASE_LISTS)
+            transformed_case_lists_data_path, SourceDataType.CBIOPORTAL_CASE_LISTS
+        )
 
     def download_s3_data(
         self, src_data_type: SourceDataType = SourceDataType.CBIOPORTAL_MUTATIONS
@@ -63,18 +63,24 @@ class CBioPortal(DownloadableDataSource):
         """
         is_mutations = src_data_type == SourceDataType.CBIOPORTAL_MUTATIONS
         data_type = "mutations" if is_mutations else "case_lists"
-        _logger.info(f"Retrieving transformed {data_type} data from s3 bucket...")
+        _logger.info("Retrieving transformed %s data from s3 bucket...", data_type)
         s3 = boto3.client("s3")
-        zip_fn = "msk_impact_2017_mutations.csv.zip" if is_mutations else "msk_impact_2017_case_lists.csv.zip"  # noqa: E501
+        zip_fn = (
+            "msk_impact_2017_mutations.csv.zip"
+            if is_mutations
+            else "msk_impact_2017_case_lists.csv.zip"
+        )
         zip_path = self.src_dir_path / zip_fn
-        with open(zip_path, "wb") as f:
-            s3.download_fileobj("vicc-normalizers",
-                                f"evidence_normalization/cbioportal/{zip_fn}", f)
+        with zip_path.open("wb") as f:
+            s3.download_fileobj(
+                "vicc-normalizers", f"evidence_normalization/cbioportal/{zip_fn}", f
+            )
         shutil.unpack_archive(zip_path, self.src_dir_path)
-        remove(zip_path)
-        _logger.info(f"Successfully downloaded transformed cBioPortal {data_type} data")
-        data_path = self.src_dir_path / zip_fn[:-4]
-        return data_path
+        Path.unlink(zip_path)
+        _logger.info(
+            "Successfully downloaded transformed cBioPortal %s data", data_type
+        )
+        return self.src_dir_path / zip_fn[:-4]
 
     def cancer_types_summary(self, hgnc_symbol: str) -> Response:
         """Get cancer types with gene mutations data
@@ -85,7 +91,7 @@ class CBioPortal(DownloadableDataSource):
         hgnc_symbol = hgnc_symbol.upper()
 
         mutation_sample_ids = set()
-        with open(self.transformed_mutations_data_path) as f:
+        with self.transformed_mutations_data_path.open() as f:
             data = csv.reader(f)
             headers = next(data)
             for row in data:
@@ -94,11 +100,12 @@ class CBioPortal(DownloadableDataSource):
                     mutation_sample_ids.add(sample_id)
 
         if not mutation_sample_ids:
-            return self.format_response(Response(data=dict(),
-                                                 source_meta_=self.source_meta))
+            return self.format_response(
+                Response(data={}, source_meta_=self.source_meta)
+            )
 
-        tumor_type_totals = dict()
-        with open(self.transformed_case_lists_data_path) as f:
+        tumor_type_totals = {}
+        with self.transformed_case_lists_data_path.open() as f:
             data = csv.reader(f)
             headers = next(data)
             for row in data:
@@ -108,11 +115,15 @@ class CBioPortal(DownloadableDataSource):
                     sample_ids = row[headers.index("case_list_ids")].split("\t")
                     tumor_type_totals[tumor_type] = {
                         "count": 0,
-                        "total": len(sample_ids)
+                        "total": len(sample_ids),
                     }
                     for sample_id in sample_ids:
                         if sample_id in mutation_sample_ids:
                             tumor_type_totals[tumor_type]["count"] += 1
-                    tumor_type_totals[tumor_type]["percent_altered"] = (tumor_type_totals[tumor_type]["count"] / tumor_type_totals[tumor_type]["total"]) * 100  # noqa: E501
-        return self.format_response(Response(data=tumor_type_totals,
-                                             source_meta_=self.source_meta))
+                    tumor_type_totals[tumor_type]["percent_altered"] = (
+                        tumor_type_totals[tumor_type]["count"]
+                        / tumor_type_totals[tumor_type]["total"]
+                    ) * 100
+        return self.format_response(
+            Response(data=tumor_type_totals, source_meta_=self.source_meta)
+        )
